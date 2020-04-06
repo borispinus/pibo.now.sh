74da7eea54cbd2c26a2e3d6ab7fc22d9f0ce4132
pinus <bvpinus@gmail.com>
2020-04-06 04:22:10 +0300

    moved style to index page

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { dom, config } from '@fortawesome/fontawesome-svg-core';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
-config.autoAddCss = false;
 
 class MyDocument extends Document {
   render() {
@@ -13,20 +10,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
             rel="stylesheet"
           />
-          <style jsx global>
-            {`
-              @import url('https://fonts.googleapis.com/css?family=Didact+Gothic');
-
-              ${dom.css()}
-              body {
-                font-family: 'Roboto', sans-serif;
-                min-height: 100vh;
-                margin: 0;
-                padding: 0;
-                position: relative;
-              }
-            `}
-          </style>
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
 import { Index as IndexPage } from '../index/Index';
 
-const Index = () => <IndexPage />;
+config.autoAddCss = false;
+
+const Index = () => (
+  <>
+    <IndexPage />
+    <style jsx global>
+      {`
+        @import url('https://fonts.googleapis.com/css?family=Didact+Gothic');
+
+        ${dom.css()}
+        body {
+          font-family: 'Roboto', sans-serif;
+          min-height: 100vh;
+          margin: 0;
+          padding: 0;
+          position: relative;
+        }
+      `}
+    </style>
+  </>
+);
 
 export default Index;
